54cd8d9f67dae53f4fbeef6e8d0f284fbcfae6ed
Brian Dang <pandabrian1@gmail.com>
2023-10-26 23:44:10 -0600

    Rotations to the Ship
    Added rotation to the player ship when the user inputs controls. The ship will fire straight and not towards the center.

--- Project RailShip/Assets/Scripts/PlayerController.cs
+++ Project RailShip/Assets/Scripts/PlayerController.cs
@@ -1,31 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float movSpeed;
+    [SerializeField] float movSpeed = 30f;
+    [SerializeField] float zRange = 6f;
+    [SerializeField] float yRange = 8f;
+
+    [SerializeField] float posPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float zThrow;
+    float yThrow;
     void Update()
     {
+        UserInput();
+        ShipRotation();
+    }
+
+    void UserInput()
+    {
         //Horizontal Control Axis
-        float zThrow = Input.GetAxis("Horizontal");
+        zThrow = Input.GetAxis("Horizontal");
         Debug.Log(zThrow);
 
         float zOffset = zThrow * movSpeed * Time.deltaTime;
-
-        float newZPos = transform.localPosition.z + zOffset;
+        float rawZPos = transform.localPosition.z + zOffset;
+        float clampedZPos = Mathf.Clamp(rawZPos, -zRange, zRange);
 
         //Vertical Contorl Axis
-        float yThrow = Input.GetAxis("Vertical");
+        yThrow = Input.GetAxis("Vertical");
         Debug.Log(yThrow);
 
         float yOffset = yThrow * movSpeed * Time.deltaTime;
-
-        float newYPos = transform.localPosition.y + yOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, 0, yRange);
 
         //Change the Position
-        transform.localPosition = new UnityEngine.Vector3(transform.localPosition.x, newYPos, newZPos);
+        transform.localPosition = new UnityEngine.Vector3(transform.localPosition.x, clampedYPos, clampedZPos);
+    }
 
+    void ShipRotation()
+    {
+        float pitchPos = transform.localPosition.y * posPitchFactor;
+        float pitchControl = yThrow * controlPitchFactor;
+
+        float pitch = pitchPos + pitchControl;
+        float yaw = transform.localPosition.z * positionYawFactor - 90;
+        float roll = zThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
